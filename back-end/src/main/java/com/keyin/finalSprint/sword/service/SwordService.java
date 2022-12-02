5ccdec1dfe4b41863bc35d518a689e0463276074
dBish6 <davidbish2002@hotmail.com>
2022-12-02 15:00:52 -0330

    Initial File Planning

--- back-end/src/main/java/com/keyin/finalSprint/sword/service/SwordService.java
+++ back-end/src/main/java/com/keyin/finalSprint/sword/service/SwordService.java
@@ -1,6 +1,6 @@
 package com.keyin.finalSprint.sword.service;
 
 // The service class is where all your "business logic" goes for your controllers. Basically, all the
-// code that you do before you lastly save or get from the database goes here.
+// code that you do before you lastly save or get from the database goes here; autowired to use class.
 public class SwordService {
 }
