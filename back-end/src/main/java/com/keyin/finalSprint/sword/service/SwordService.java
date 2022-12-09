a0a05f59cfcc8dbed6c4354a688f796b6b96ebf2
Dominic Whelan <dominic.whelan27@gmail.com>
2022-12-09 00:24:52 -0330

    SwordService implementation, minor edits of Sword Controller

--- back-end/src/main/java/com/keyin/finalSprint/sword/service/SwordService.java
+++ back-end/src/main/java/com/keyin/finalSprint/sword/service/SwordService.java
@@ -1,6 +1,47 @@
 package com.keyin.finalSprint.sword.service;
 
+import com.keyin.finalSprint.sword.exceptions.SwordNotFoundException;
+import com.keyin.finalSprint.sword.model.Sword;
+import com.keyin.finalSprint.sword.respository.SwordRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 // The service class is where all your "business logic" goes for your controllers. Basically, all the
 // code that you do before you lastly save or get from the database goes here; autowired to use class.
+@Service
 public class SwordService {
+
+    @Autowired
+    SwordRepository swordRepo;
+
+    public Sword serveSword(String id){
+        Optional<Sword> swordReturned = swordRepo.findById(Long.parseLong(id));
+        Sword swordFound;
+        if(swordReturned.isPresent()){
+            swordFound = swordReturned.get();
+            return swordFound;
+        } else {
+            throw new SwordNotFoundException(Long.parseLong(id));
+        }
+    }
+
+    public void replaceSword(Sword swordReplacement, String id){
+        Optional<Sword> swordReturned = swordRepo.findById(Long.parseLong(id));
+        Sword swordUpdated;
+        if(swordReturned.isPresent()){
+            swordUpdated = swordReturned.get();
+            swordUpdated.setDescription(swordReplacement.getDescription());
+            swordUpdated.setType(swordReplacement.getType());
+            swordUpdated.setLength(swordReplacement.getLength());
+            swordUpdated.setMass(swordReplacement.getMass());
+            swordUpdated.setName(swordReplacement.getName());
+            swordUpdated.setPrice(swordReplacement.getPrice());
+            swordUpdated.setImage_url(swordReplacement.getImage_url());
+            swordRepo.save(swordUpdated);
+        } else {
+            throw new SwordNotFoundException(Long.parseLong(id));
+        }
+    }
 }
