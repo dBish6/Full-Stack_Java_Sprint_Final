2fd13e4368f507df0d5818a3b5c9a06a8a7bb1c2
dBish6 <davidbish2002@hotmail.com>
2022-12-16 12:39:30 -0330

    User class working sort of.

--- back-end/src/main/java/com/keyin/finalSprint/user/controller/UserController.java
+++ back-end/src/main/java/com/keyin/finalSprint/user/controller/UserController.java
@@ -1,17 +1,19 @@
 package com.keyin.finalSprint.user.controller;
 
+
 import com.keyin.finalSprint.user.model.User;
 import com.keyin.finalSprint.user.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin
-@RestController
+@Controller
 @RequestMapping("/api/")
 public class UserController {
     @Autowired
@@ -19,38 +21,36 @@ public class UserController {
 
     @GetMapping("/users")
     public List<User> listUsers() {
-
         return userRepo.findAll();
+    }
+
+    @PostMapping("/login")
+    public void addNote(@RequestBody User user, HttpServletRequest request) {
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) request.getSession().getAttribute("SESSION_MESSAGE");
+        if(messages == null) {
+            messages = new ArrayList<>();
+            request.getSession().setAttribute("SESSION", messages);
+        } else {
+            messages.add(Long.toString(user.getId()));
+            request.getSession().setAttribute("SESSION", messages);
+        }
+    }
+
+    @PostMapping("/logout")
+    public void destroySession(HttpServletRequest request) {
+        request.getSession().invalidate();
 
     }
 
-//    @PostMapping("/login")
-//    public void addNote(@RequestBody User user, HttpServletRequest request) {
-//        List<String> sessions = (List<String>) request.getSession().getAttribute("SESSION");
-//        if(sessions == null) {
-//            sessions = new ArrayList<>();
-//            request.getSession().setAttribute("SESSION", user.getId());
-//        } else {
-//            sessions.add(Long.toString(user.getId()));
-//            request.getSession().setAttribute("SESSION", user.getId());
-//
-//        }
-//
-//    }
-//    @PostMapping("/logout")
-//    public void destroySession(HttpServletRequest request) {
-//        request.getSession().invalidate();
-//
-//    }
-//
-//    @PostMapping("/register")
-//    public User processRegister(User user) {
-//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//        String encodedPassword = passwordEncoder.encode(user.getPassword());
-//        user.setPassword(encodedPassword);
-//
-//       return userRepo.save(user);
-//
-//    }
+    @PostMapping("/register")
+    public User processRegister(@RequestBody User user) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+
+       return userRepo.save(user);
+
+    }
 
 }
